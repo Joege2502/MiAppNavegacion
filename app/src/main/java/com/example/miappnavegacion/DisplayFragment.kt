5f3ede0dad079b253eef7e5127a07e8886b37213
Joege2502 <jorgeososres9@gmail.com>
2025-08-22 19:08:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/miappnavegacion/DisplayFragment.kt
@@ -0,0 +1,20 @@
+package com.example.miappnavegacion
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class DisplayFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_display, container, false)
+        val displayTextView: TextView = view.findViewById(R.id.text_view_display)
+        val userName = activity?.intent?.getStringExtra("USER_NAME")
+        displayTextView.text = "Hola, $userName!"
+        return view
+    }
+}
